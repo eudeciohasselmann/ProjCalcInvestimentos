3ff118b6ac757c4320fb111237276d9ee6023c0d
eudeciohasselmann <eudecio@gmail.com>
2025-10-21 19:54:03 -0300

    feat: implement investment form functionality and return calculation

---
+++ main.js
@@ -0,0 +1,33 @@
+import { generateReturnsArray } from "./src/investimentGoals.js";
+
+const form = document.getElementById("investment-form");
+
+function renderProgression(evt) {
+  evt.preventDefault();
+  const startingAmount = Number(
+    document.getElementById("starting-amount").value.replace(",", ".")
+  );
+  const additionalContribution = Number(
+    document.getElementById("additional-contribution").value.replace(",", ".")
+  );
+  const timeAmount = Number(document.getElementById("time-amount").value);
+  const timeAmountPeriod = document.getElementById("time-amount-period").value;
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
+  const returnRatePeriod = document.getElementById("evaluation-period").value;
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
+
+  const returnsArray = generateReturnsArray(
+    startingAmount,
+    timeAmount,
+    timeAmountPeriod,
+    additionalContribution,
+    returnRate,
+    returnRatePeriod
+  );
+}
+
+form.addEventListener("submit", renderProgression);
