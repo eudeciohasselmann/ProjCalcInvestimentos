0a773147b8df307db2493da2d91a3a2cb43e17d6
eudeciohasselmann <eudecio@gmail.com>
2025-10-21 20:34:56 -0300

    feat: add form validation and clear functionality

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import { generateReturnsArray } from "./src/investimentGoals.js";
 
 const form = document.getElementById("investment-form");
+const clearFormButton = document.getElementById("clear-form");
 
 function renderProgression(evt) {
   evt.preventDefault();
+
+  if (document.querySelector(".error")) {
+    alert("Por favor, corrija os erros no formulário antes de enviar.");
+    return;
+  }
   const startingAmount = Number(
     document.getElementById("starting-amount").value.replace(",", ".")
   );
@@ -30,4 +36,56 @@ function renderProgression(evt) {
   );
 }
 
+function clearForm() {
+  form["starting-amount"].value = "";
+  form["additional-contribution"].value = "";
+  form["time-amount"].value = "";
+  form["return-rate"].value = "";
+  form["tax-rate"].value = "";
+
+  const errorInputContainers = document.querySelectorAll(".error");
+
+  for (const errorInputContainer of errorInputContainers) {
+    errorInputContainer.classList.remove("error");
+    errorInputContainer.parentElement.querySelector("p").remove();
+  }
+}
+
+function validateInput(evt) {
+  if (evt.target.value === "") {
+    return;
+  }
+
+  const { parentElement } = evt.target;
+  const grandParentElement = evt.target.parentElement.parentElement;
+  const inputValue = evt.target.value.replace(",", ".");
+
+  if (
+    !parentElement.classList.contains("error") &&
+    (isNaN(inputValue) || Number(inputValue) <= 0)
+  ) {
+    // objetivo: <p class="text-red-500">Insira um valor numérico e maior que zero</p>
+    const errorTextElement = document.createElement("p"); //<p></p>
+    errorTextElement.classList.add("text-red-500"); //<p class='text-red-500'></p>
+    errorTextElement.innerText = "Insira um valor numérico e maior que zero"; //<p class="text-red-500">Insira um valor numérico e maior que zero</p>
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(errorTextElement);
+  } else if (
+    parentElement.classList.contains("error") &&
+    !isNaN(inputValue) &&
+    Number(inputValue) > 0
+  ) {
+    parentElement.classList.remove("error");
+    grandParentElement.querySelector("p").remove();
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
+}
+
 form.addEventListener("submit", renderProgression);
+clearFormButton.addEventListener("click", clearForm);
